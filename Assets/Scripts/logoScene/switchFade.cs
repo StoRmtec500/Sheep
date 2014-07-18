e12b794c7b4125d95916d6cf53abf3b618a3b281
StoRmtec500 <martin@kuenz.co.at>
2014-07-18 11:33:58 +0200

    NGUI hinzugefügt

---
+++ Assets/Scripts/logoScene/switchFade.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchFade : MonoBehaviour {
+
+	public TweenAlpha logo;
+	bool finish;
+	public UI2DSprite logoGO;
+
+	public void Start()
+	{
+		Invoke ("loadLogo", 0f);
+		}
+
+	void Update()
+	{
+		if (finish == true) {
+			Invoke("chanceScene", 1f);
+				}
+		}
+
+	public void playReverse()
+	{
+				this.logo.PlayReverse ();
+				if (logo.value == 0) {
+						finish = true;
+				}
+		}
+	// Use this for initialization
+	public void chanceScene()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+		}
+
+	void loadLogo()
+	{
+		this.logoGO.alpha = 0f;
+	}
+}
---
+++ Assets/Scripts/mainMenuScene/btnPlay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnPlay : MonoBehaviour {
+
+	public GameObject btnPlayGoj;
+
+	public int sceneIndex;
+	public UILabel progressText;
+
+
+	void Start()
+	{
+		UIEventListener.Get (btnPlayGoj).onClick += LoadLevel;
+	}
+
+	void LoadLevel (GameObject go)
+	{
+		InvokeRepeating("LoadGame", 0f, 2f);
+	}
+
+	void LoadGame()
+	{
+		//the game scene is ready
+		if (Application.CanStreamedLevelBeLoaded(sceneIndex))
+		{
+			//set progress to 100, so that gets displayed while switching scenes 
+			progressText.text = 100 + "%";
+			//load the game scene
+			Application.LoadLevel(sceneIndex);
+		}
+		else
+		{
+			//we're still loading the game scene, get the current progress and store it
+			progressText.text = ((int)(Application.GetStreamProgressForLevel(sceneIndex) * 100)) + "%";
+		}
+	}
+}
