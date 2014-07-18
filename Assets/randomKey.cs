a782fcba4336e4c0be72652a20b78d6ffaf128f2
StoRmtec500 <martin@kuenz.co.at>
2014-07-18 08:42:30 +0200

    main

---
+++ Assets/randomKey.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class randomKey : MonoBehaviour {
+
+	static randomKey myInstance;
+	public static randomKey Instance { get { return myInstance; } }
+	public int zahl;
+	public bool start = false;
+	public bool erlaubt = false;
+	public bool einmal = false;
+	bool gestartet = false;
+	public enum startState {On, Off };
+	public startState starts = startState.Off;
+	public GUIText zahlanzeige;
+	public GUIText numberklicktanzeige;
+	public int[] nummer = new int[7] {0,1,2,3,4,5,6};
+	public GUISkin mySkin;
+	public GameObject[] button;
+	public int buttonstring;
+	//public static int buttonnumber;
+
+
+	void Start()
+	{
+				myInstance = this;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (start) 
+		{
+			zahlstart ();
+		}
+
+		zahlanzeige.text = zahl.ToString ();
+		numberklicktanzeige.text = buttonstring.ToString ();
+
+
+		if (einmal == true) {
+			if (buttonstring == zahl && erlaubt == true) {
+				Debug.Log ("--------------WEITER--------------"); 
+				erlaubt = false;
+				einmal = false;
+			}
+			if (buttonstring != zahl && erlaubt == true) {
+				erlaubt = false;
+				einmal = false;
+				Time.timeScale = 0f;
+				Debug.Log("------------- Aus ---------------");
+			}
+		}
+	}
+
+	void zahlstart()
+	{
+		start = false;
+		gestartet = true;
+		starts = startState.On;
+		StartCoroutine (startZahl (1.5f));
+	}
+
+	IEnumerator startZahl(float timer)
+	{
+		while (starts == startState.On) 
+		{
+			zahl = Random.Range(1, nummer.Length);		
+			yield return new WaitForSeconds (timer);
+			Debug.Log (nummer[zahl]);
+
+			if (gestartet == true) {
+				Time.timeScale = 0f;
+				Debug.Log("------------- NICHTS GEDRÜCKT ---------------");
+			}
+		}
+	}
+
+	// Playbutton wird angezeigt ---
+	void OnGUI()
+	{
+		if(GUI.Button (new Rect (10, 10, 50, 20), "Play", mySkin.button))
+			startPlaying();
+	}
+
+	// Zahlen werden angezeigt ----
+	void startPlaying ()
+	{
+		start = true;
+	}
+
+}
---
+++ Assets/touch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touch : MonoBehaviour {
+
+	static touch myInstance;                       //Hold a reference to this script
+	public static touch Instance { get { return myInstance; } }
+
+	public GameObject button;
+	public int nummertouch;
+
+	// Use this for initialization
+	void Start () {
+		myInstance = this;
+	}
+
+	void Update() {
+
+		}
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+
+		nummertouch = System.Int32.Parse (gameObject.name);
+		Debug.Log ("Buttonname: " + nummertouch.ToString());
+		randomKey.Instance.buttonstring = 0;
+		randomKey.Instance.erlaubt = true;
+		randomKey.Instance.einmal = true;
+		randomKey.Instance.buttonstring += nummertouch;
+		//randomKey.Instance.einmal = false;
+	}
+}
